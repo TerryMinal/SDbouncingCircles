69d80b67d58c0179849ac9b2a3863bf8e774af29
TerryMinal <terryguan513@gmail.com>
2018-03-10 14:03:45 -0500

    animation works

--- svg.js
+++ svg.js
@@ -1,12 +1,15 @@
 var pic = document.getElementById("vimage");
 var clearBtn = document.getElementById("clear")
 var dots = [];
-
+var intervalID;
+var beginAnimation = true;
 //clears the screen
 var clearScreen = function(e){
+    clearInterval(intervalID);
     dots.forEach((a) => a.element.remove());
     dots = [];
-    console.log("Cleared Screen.")
+    beginAnimation = true;
+    console.log("Cleared Screen.");
 }
 
 var makeDot = function(x, y) {
@@ -14,19 +17,19 @@ var makeDot = function(x, y) {
     return {
         x : x,
         y : y,
-        dx : Math.random() * 10,
-        dy : Math.random() * 10,
+        dx : Math.pow(-1, Math.round(Math.random() * 4)) * Math.random() * 10,
+        dy : Math.pow(-1, Math.round(Math.random() * 4)) * Math.random() * 10,
         r : 15,
-        fill : "red",
+        fill : "#9A98C0",
         element : cl,
         move : function() {
             this.x += this.dx;
             this.y += this.dy;
-            if (x < r || x > 500 - r) {
-                dx *= -1;
+            if (x <= this.r || x >= 500 - this.r) {
+                this.dx *= -1;
             }
-            if (y < r || y > 500 - r) {
-                dy *= -1;
+            if (y <= this.r || y >= 500 - this.r) {
+                this.dy *= -1;
             }
 
         },
@@ -35,7 +38,6 @@ var makeDot = function(x, y) {
             cl.setAttribute("cy", this.y);
             cl.setAttribute("r", this.r);
             cl.setAttribute("fill", this.fill);
-            cl.addEventListener('click', circle_clicked);
             pic.appendChild(cl);
         }
     };
@@ -43,13 +45,23 @@ var makeDot = function(x, y) {
 
 //a blank space on svg container is clicked -- make circle
 var svg_clicked = function(e){
+    if (beginAnimation) {
+      intervalID = setInterval(animate, 20);
+      beginAnimation = !beginAnimation;
+    }
     if (e.target == this){  //<-- toElement is undefined
         console.log("coords: ", e.offsetX, ", ", e.offsetY);
         var dot = makeDot(e.offsetX,e.offsetY);
         dots.push(dot);
-        dot.display();
+        // dot.display();
     };
 }
+
+var animate = function() {
+  dots.forEach((a) => a.move());
+  dots.forEach((a) => a.display());
+}
+
 
 clearBtn.addEventListener("click", clearScreen)
 pic.addEventListener("click", svg_clicked)
